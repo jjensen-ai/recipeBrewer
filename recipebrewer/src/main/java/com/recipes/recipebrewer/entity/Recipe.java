75f72c7142ee2b4ac8a6194d32c9991417c1522d
Jrdc12 <juliodc289@gmail.com>
2022-10-22 04:16:26 -0400

    Added Rest API (Unfinished) and Database (Unfinished)

---
+++ recipebrewer/src/main/java/com/recipes/recipebrewer/entity/Recipe.java
@@ -0,0 +1,22 @@
+package com.recipes.recipebrewer.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "recipes")
+@Getter
+@Setter
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    private long id;
+    private String ingredient;
+    private String cookTime;
+
+}
---
+++ recipebrewer/src/main/java/com/recipes/recipebrewer/entity/User.java
@@ -0,0 +1,28 @@
+package com.recipes.recipebrewer.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
