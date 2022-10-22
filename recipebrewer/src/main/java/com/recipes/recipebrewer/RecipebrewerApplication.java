128e7fb59804e00b1431af92ae69258b96a4165f
jjensen-ai <jordon.jensen@live.ca>
2022-10-21 21:23:59 -0400

    commit with initialized project

---
+++ recipebrewer/src/main/java/com/recipes/recipebrewer/RecipebrewerApplication.java
@@ -0,0 +1,13 @@
+package com.recipes.recipebrewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipebrewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipebrewerApplication.class, args);
+	}
+
+}
